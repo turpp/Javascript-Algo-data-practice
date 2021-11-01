f5d0171a4ba12ad1009b39ab77f3719963b5199f
turpp <white.tripp@icloud.com>
2021-10-31 21:54:53 -0500

    radix sort. way to sort with out comparing elements

--- practiceProblems.js
+++ practiceProblems.js
@@ -752,3 +752,41 @@ function mergeSort(array){
 
     return mergeArrays(left, right)
 }
+
+
+//======Radix Sort===========
+//used for a sorting a list of numbers
+//can convert other things to numbers and do same sort but has to be in number format
+//it does not compare elements together like the other sorts I have done
+//it uses the last digit in each number and sorts then goes to next digit
+function getDigit(num, i) {
+    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+  }
+  
+  function digitCount(num) {
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+  }
+  
+  function mostDigits(nums) {
+    let maxDigits = 0;
+    for (let i = 0; i < nums.length; i++) {
+      maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+    }
+    return maxDigits;
+  }
+  
+  function radixSort(nums){
+      let maxDigitCount = mostDigits(nums);
+      for(let k = 0; k < maxDigitCount; k++){
+          let digitBuckets = Array.from({length: 10}, () => []);
+          for(let i = 0; i < nums.length; i++){
+              let digit = getDigit(nums[i],k);
+              digitBuckets[digit].push(nums[i]);
+          }
+          nums = [].concat(...digitBuckets);
+      }
+      return nums;
+  }
+  
+  radixSort([23,345,5467,12,2345,9852])
