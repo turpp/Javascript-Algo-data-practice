2e3d48b7ad71ce664c6878ca221f41f2d9a5cc02
turpp <white.tripp@icloud.com>
2021-10-31 22:14:15 -0500

    added quick sort

--- practiceProblems.js
+++ practiceProblems.js
@@ -790,3 +790,46 @@ function getDigit(num, i) {
   }
   
   radixSort([23,345,5467,12,2345,9852])
+
+
+
+  //===========Quick Sort================
+  //Faster than bubble, selection and insertion sort
+  //similar to merge sort with the fact that a one element array is already sorted
+  //works by selecting a pivot point and finding the index where it should be in the sorted array
+  //once pivot point is in correct position can then quick sort on either side of pivot
+
+  function pivot(arr, start = 0, end = arr.length - 1) {
+    const swap = (arr, idx1, idx2) => {
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+  
+    // We are assuming the pivot is always the first element
+    let pivot = arr[start];
+    let swapIdx = start;
+  
+    for (let i = start + 1; i <= end; i++) {
+      if (pivot > arr[i]) {
+        swapIdx++;
+        swap(arr, swapIdx, i);
+      }
+    }
+  
+    // Swap the pivot from the start the swapPoint
+    swap(arr, start, swapIdx);
+    return swapIdx;
+  }
+  
+  
+  function quickSort(arr, left = 0, right = arr.length -1){
+      if(left < right){
+          let pivotIndex = pivot(arr, left, right) //3
+          //left
+          quickSort(arr,left,pivotIndex-1);
+          //right
+          quickSort(arr,pivotIndex+1,right);
+        }
+       return arr;
+  } 
+             
+  quickSort([100,-3,2,4,6,9,1,2,5,3,23])
